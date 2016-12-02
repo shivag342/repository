7266b45023d04990cf7b362cd34ed697268a5230
shivag342 <naveeng342@gmail.com>
2016-12-02 11:47:07 +0530

    azuredscfiletest

---
+++ Provision-VM.ps1
@@ -0,0 +1,18 @@
+﻿Configuration FileResourceDemo
+{
+    Node "localhost"
+    {
+        File CreateFile {
+            DestinationPath = 'C:\Temp\Test.txt'
+            Ensure = "Present"
+            Contents = 'Hello World!'
+        }
+
+        Log AfterDirectoryCopy
+        {
+            # The message below gets written to the Microsoft-Windows-Desired State Configuration/Analytic log
+            Message = "Finished running the file resource with ID CreateFile"
+            DependsOn = "[File]CreateFile" # This means run "CreateFile" first.
+        }
+    }
+}
